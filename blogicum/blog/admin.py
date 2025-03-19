fa6e4d6ccaf216f2f0a85be1de4622a154a86f42
VitalyMigaev <kitalyans1@gmail.com>
2025-03-19 13:43:14 +0300

    Update admin.py

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post, Category, Location
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'pub_date', 'is_published', 'category', 'location')
     list_filter = ('is_published', 'pub_date', 'category')
