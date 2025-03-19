67e0c03f40e653c15c601bc99cc4240c642f3641
VitalyMigaev <kitalyans1@gmail.com>
2025-03-19 13:44:39 +0300

    Update admin.py

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -3,9 +3,13 @@ from .models import Post, Category, Location
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'pub_date', 'is_published', 'category', 'location')
+    list_display = (
+        'title', 'author', 'pub_date', 
+        'is_published', 'category', 'location'
+    )
     list_filter = ('is_published', 'pub_date', 'category')
     search_fields = ('title', 'text')
+
 
 admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
