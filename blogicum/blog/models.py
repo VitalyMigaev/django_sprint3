44dcd2a9b6e2adb4657d1d40a4535b46954b7396
VitalyMigaev <kitalyans1@gmail.com>
2025-03-19 13:49:56 +0300

    Update models.py

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-User = get_user_model()
+User  = get_user_model()
 
 
 class Category(models.Model):
@@ -10,16 +10,18 @@ class Category(models.Model):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.'
-        )
+        help_text='Идентификатор страницы для URL; разрешены символы латиницы, '
+                  'цифры, дефис и подчёркивание.'
+    )
     is_published = models.BooleanField(
         default=True,
         verbose_name='Опубликовано',
-        help_text='Снимите галочку, чтобы скрыть публикацию.')
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Добавлено'
-        )
+    )
 
     class Meta:
         verbose_name = 'категория'
@@ -33,16 +35,16 @@ class Location(models.Model):
     name = models.CharField(
         max_length=256,
         verbose_name='Название места'
-        )
+    )
     is_published = models.BooleanField(
         default=True,
         verbose_name='Опубликовано',
         help_text='Снимите галочку, чтобы скрыть публикацию.'
-        )
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Добавлено'
-        )
+    )
 
     class Meta:
         verbose_name = 'местоположение'
@@ -57,33 +59,35 @@ class Post(models.Model):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.'
-        )
+        help_text='Если установить дату и время в будущем — можно делать '
+                  'отложенные публикации.'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         verbose_name='Автор публикации'
-        )
+    )
     location = models.ForeignKey(
         Location,
         null=True,
         on_delete=models.SET_NULL,
         verbose_name='Местоположение'
-        )
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
-        null=True, verbose_name='Категория'
-        )
+        null=True,
+        verbose_name='Категория'
+    )
     is_published = models.BooleanField(
         default=True,
         verbose_name='Опубликовано',
         help_text='Снимите галочку, чтобы скрыть публикацию.'
-        )
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Добавлено'
-        )
+    )
 
     class Meta:
         verbose_name = 'публикация'
