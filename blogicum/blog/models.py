2d6f1f34e819a1c640b766003a5d56d4b252c7df
VitalyMigaev <kitalyans1@gmail.com>
2025-03-19 14:03:46 +0300

    Update models.py

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -10,8 +10,10 @@ class Category(models.Model):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; разрешены символы латиницы, '
-                  'цифры, дефис и подчёркивание.'
+        help_text=(
+            'Идентификатор страницы для URL; разрешены символы латиницы, '
+            'цифры, дефис и подчёркивание.'
+        )
     )
     is_published = models.BooleanField(
         default=True,
